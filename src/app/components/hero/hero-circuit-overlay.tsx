5a82d30bb4267cbf818cc5f36e302d4faf122685
rakib <rahmed@hvac.mea.com>
2026-06-08 02:06:23 -0400

    Hero Circuits on Chrome

--- src/app/components/hero/hero-circuit-overlay.tsx
+++ src/app/components/hero/hero-circuit-overlay.tsx
@@ -26,7 +26,11 @@ export default function HeroCircuitOverlay({
   const [isMounted, setIsMounted] = useState(false);
   const glowFilter = blur ? `url(#piratechsCircuitGlow)` : undefined;
   const isChromeOrPwaDevice = isMounted && Boolean(
-    !isPWA && (platform && (!platform?.mobile && !platform?.ios && platform?.chrome && !platform?.os?.toLowerCase()?.includes(`mac`)))
+    !isPWA && (platform && platform?.chrome && (
+      !platform?.mobile && !platform?.ios && !platform?.os?.toLowerCase()?.includes(`mac`)
+    ) || (
+      platform?.os?.toLowerCase()?.includes(`windows`)
+    ))
   );
   const shouldRenderCircuit = showCircuitOverlay && isChromeOrPwaDevice;
 
